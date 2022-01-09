0da422d6cf07b335b790a4bd7129c13d49763812
DenisMoskalenko <Ulibocka123@gmail.com>
2022-01-09 05:36:39 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 1341; // Стоимость билета в рублях
+        int costOfOneMile = 20; // Количество рублей для расчёта одной бонусной мили
+
+        System.out.println((ticketPrice / costOfOneMile) + " бонусных миль(и)");
+    }
+}
